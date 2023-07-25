7374a022202cfff445765a61dadf59585815a4d8
MaxAlieksieiev <corp.workMA@gmail.com>
2023-07-25 11:47:30 +0300

    add stepper

--- src/pages/MultiForm/components/UserDetails/UserDetails.tsx
+++ src/pages/MultiForm/components/UserDetails/UserDetails.tsx
@@ -1,1 +1,12 @@
-export const UserDetails = () => <div>UserDetails</div>;
+import { Button } from '@mui/material';
+
+import { FC } from 'react';
+
+export interface IUserDetailsProps {
+  nextStep: () => void,
+}
+
+export const UserDetails: FC<IUserDetailsProps> = ({ nextStep }) => {
+
+  return <Button onClick={nextStep}>Next</Button>;
+};
